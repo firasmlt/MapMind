403738598b17166c0d7045a96a2f30c4697f5582
unknown <mellitifiras@protonmail.com>
2022-08-19 20:33:22 +0100

    adding the toolbar component

--- client/src/components/ToolBar.js
+++ client/src/components/ToolBar.js
@@ -1,7 +1,20 @@
 import styles from "./ToolBar.module.css";
+import Chart from "../images/Chart.svg";
+import Circle from "../images/circle_left.svg";
+import Trash from "../images/Trash_duotone_line.svg";
 
 function ToolBar() {
-  return <div className={styles.toolbar}>toolbar</div>;
+  return (
+    <div className={styles.toolbar}>
+      <input type="text" />
+      <div className={styles.tools}>
+        <img src={Chart} width="38px" />
+        <img src={Circle} width="38px" />
+        <input type="checkbox" />
+        <img src={Trash} width="38px" />
+      </div>
+    </div>
+  );
 }
 
 export default ToolBar;
